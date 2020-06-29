c110c525902f4f48608e0b290556fa9fff2bec77
Niklas Rämö <inramo@gmail.com>
2020-06-29 21:46:07 +0300

    Simplify Animator.js as some of it's optimizations are not needed anymore.

--- src/Animator/Animator.js
+++ src/Animator/Animator.js
@@ -4,7 +4,6 @@
  * https://github.com/haltu/muuri/blob/master/LICENSE.md
  */
 
-import getCurrentStyles from '../utils/getCurrentStyles';
 import getUnprefixedPropName from '../utils/getUnprefixedPropName';
 import isFunction from '../utils/isFunction';
 import isNative from '../utils/isNative';
@@ -22,11 +21,7 @@ var HAS_NATIVE_WEB_ANIMATIONS = !!(Element && isNative(Element.prototype.animate
 function Animator(element) {
   this._element = element;
   this._animation = null;
-  this._duration = 0;
-  this._easing = '';
   this._callback = null;
-  this._props = [];
-  this._values = [];
   this._isDestroyed = false;
   this._onFinish = this._onFinish.bind(this);
 }
@@ -62,76 +57,23 @@ Animator.prototype.start = function (propsFrom, propsTo, options) {
     return;
   }
 
-  var animation = this._animation;
-  var currentProps = this._props;
-  var currentValues = this._values;
-  var duration = opts.duration || 300;
-  var easing = opts.easing || 'ease';
-  var cancelAnimation = false;
-  var propName, propCount, propIndex;
-
-  // If we have an existing animation running, let's check if it needs to be
-  // cancelled or if it can continue running.
-  if (animation) {
-    propCount = 0;
-
-    // Cancel animation if duration or easing has changed.
-    if (duration !== this._duration || easing !== this._easing) {
-      cancelAnimation = true;
-    }
-
-    // Check if the requested animation target props and values match with the
-    // current props and values.
-    if (!cancelAnimation) {
-      for (propName in propsTo) {
-        ++propCount;
-        propIndex = currentProps.indexOf(propName);
-        if (propIndex === -1 || propsTo[propName] !== currentValues[propIndex]) {
-          cancelAnimation = true;
-          break;
-        }
-      }
-
-      // Check if the target props count matches current props count. This is
-      // needed for the edge case scenario where target props contain the same
-      // styles as current props, but the current props have some additional
-      // props.
-      if (propCount !== currentProps.length) {
-        cancelAnimation = true;
-      }
-    }
-  }
-
-  // Cancel animation (if required).
-  if (cancelAnimation) animation.cancel();
+  // Cancel existing animation.
+  if (this._animation) this._animation.cancel();
 
   // Store animation callback.
   this._callback = isFunction(opts.onFinish) ? opts.onFinish : null;
 
-  // If we have a running animation that does not need to be cancelled, let's
-  // call it a day here and let it run.
-  if (animation && !cancelAnimation) return;
-
-  // Store target props and values to instance.
-  currentProps.length = currentValues.length = 0;
-  for (propName in propsTo) {
-    currentProps.push(propName);
-    currentValues.push(propsTo[propName]);
-  }
-
   // Start the animation. We need to provide unprefixed property names to the
   // Web Animations polyfill if it is being used. If we have native Web
   // Animations available we need to provide prefixed properties instead.
-  this._duration = duration;
-  this._easing = easing;
   this._animation = element.animate(
     [
-      createFrame(propsFrom, HAS_NATIVE_WEB_ANIMATIONS),
-      createFrame(propsTo, HAS_NATIVE_WEB_ANIMATIONS),
+      createKeyframe(propsFrom, HAS_NATIVE_WEB_ANIMATIONS),
+      createKeyframe(propsTo, HAS_NATIVE_WEB_ANIMATIONS),
     ],
     {
-      duration: duration,
-      easing: easing,
+      duration: opts.duration || 300,
+      easing: opts.easing || 'ease',
     }
   );
   this._animation.onfinish = this._onFinish;
@@ -150,17 +92,6 @@ Animator.prototype.stop = function () {
   if (this._isDestroyed || !this._animation) return;
   this._animation.cancel();
   this._animation = this._callback = null;
-  this._props.length = this._values.length = 0;
-};
-
-/**
- * Read the current values of the element's animated styles from the DOM.
- *
- * @public
- * @return {Object}
- */
-Animator.prototype.getCurrentStyles = function () {
-  return getCurrentStyles(element, currentProps);
 };
 
 /**
@@ -198,7 +129,6 @@ Animator.prototype.destroy = function () {
 Animator.prototype._onFinish = function () {
   var callback = this._callback;
   this._animation = this._callback = null;
-  this._props.length = this._values.length = 0;
   callback && callback();
 };
 
@@ -207,7 +137,7 @@ Animator.prototype._onFinish = function () {
  * ***************
  */
 
-function createFrame(props, prefix) {
+function createKeyframe(props, prefix) {
   var frame = {};
   for (var prop in props) {
     frame[prefix ? prop : getUnprefixedPropName(prop)] = props[prop];

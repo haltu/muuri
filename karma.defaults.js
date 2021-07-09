87481bacd52fed72230f8131a9efee3bb5e34068
Niklas Rämö <inramo@gmail.com>
2021-07-09 22:03:53 +0300

    Update dependencies.

--- karma.defaults.js
+++ karma.defaults.js
@@ -3,15 +3,7 @@ const pkg = require('./package.json');
 module.exports = {
   basePath: '',
   frameworks: ['qunit'],
-  plugins: [
-    'karma-qunit',
-    'karma-chrome-launcher',
-    'karma-firefox-launcher',
-    'karma-safari-launcher',
-    'karma-edge-launcher',
-    'karma-sauce-launcher',
-    'karma-story-reporter'
-  ],
+  plugins: ['karma-qunit', 'karma-chrome-launcher', 'karma-sauce-launcher', 'karma-story-reporter'],
   files: [
     './node_modules/web-animations-js/web-animations.min.js',
     './node_modules/prosthetic-hand/dist/prosthetic-hand.js',
@@ -23,7 +15,7 @@ module.exports = {
     './tests/grid-options/*.js',
     './tests/grid-methods/*.js',
     './tests/grid-events/*.js',
-    './tests/item-methods/*.js'
+    './tests/item-methods/*.js',
   ],
   reporters: ['story', 'saucelabs'],
   colors: true,
@@ -32,32 +24,32 @@ module.exports = {
   singleRun: true,
   hostname: '127.0.0.1',
   sauceLabs: {
-    testName: pkg.name + ' - ' + pkg.version + ' - unit tests'
+    testName: pkg.name + ' - ' + pkg.version + ' - unit tests',
   },
   customLaunchers: {
     slChrome: {
       base: 'SauceLabs',
       browserName: 'chrome',
       platform: 'Windows 10',
-      version: 'latest'
+      version: 'latest',
     },
     slFirefox: {
       base: 'SauceLabs',
       browserName: 'firefox',
       platform: 'Windows 10',
-      version: 'latest'
+      version: 'latest',
     },
     slSafari: {
       base: 'SauceLabs',
       browserName: 'safari',
       platform: 'macOS 10.12',
-      version: 'latest'
+      version: 'latest',
     },
     slEdge: {
       base: 'SauceLabs',
       browserName: 'MicrosoftEdge',
       platform: 'Windows 10',
-      version: 'latest'
-    }
-  }
+      version: 'latest',
+    },
+  },
 };
